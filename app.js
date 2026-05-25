97a95d12fe452134acc2b3bbdfeb23f8edc98bb1
Har1528 <harikrishnanc206@gmail.com>
2026-05-25 15:46:43 +0530

    Fix winning patterns and improve game logic

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let newgame=document.querySelector("#newgame");
 let msg=document.querySelector('#msg');
 let turn0=true;
 const winptr=[
-    [0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[3,4,5],[6,7,8],[2,4,8]
+    [0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[3,4,5],[6,7,8],[2,4,6]
 ];
 boxes.forEach((box)=>{
     box.addEventListener("click",()=>{
